843f0d48a57c993133a5b3c77d86d08f04ded38c
othmane <othmane.alaouimekkaoui@gmail.com>
2023-11-18 04:54:05 -0800

    task 1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,24 @@
 #include "sort.h"
 
-/*
+/**
+ * swap_nodes - swap two nodes in list
+ * @node_a: first node
+ * @node_b: second node
+ * Return: nothing
+ */
+void swap_nodes(listint_t *node_a, listint_t *node_b)
+{
+	if (node_a->prev)
+		node_a->prev->next = node_b;
+	if (node_b->next)
+		node_b->next->prev = node_a;
+	node_a->next = node_b->next;
+	node_b->prev = node_a->prev;
+	node_a->prev = node_b;
+	node_b->next = node_a;
+}
+
+/**
  * insertion_sort_list - sorts a doubly linked list of integers
  * @list: the list to sort
  * Return: void
@@ -8,22 +26,27 @@
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current = (*list)->next;
-	listint_t *tmp;
-	int tp;
+	listint_t *nodeA;
+	listint_t *nodeB;
 
-	while (current)
+	if (!list || !*list || !(*list)->next)
+		return;
+	nodeA = (*list)->next;
+	while (nodeA)
 	{
-		tmp = current->prev;
-		while (tmp && tmp->n > current->n)
+		nodeB = nodeA;
+		nodeA = nodeA->next;
+		while (nodeB && nodeB->prev)
 		{
-			tp = tmp->n;
-			tmp->n = current->n;
-			current->n = tp;
-
-			print_list(*list);
-			tmp = tmp->prev;
+			if (nodeB->prev->n > nodeB->n)
+			{
+				swap_nodes(nodeB->prev, nodeB);
+				if (!nodeB->prev)
+					*list = nodeB;
+				print_list((const listint_t *)*list);
+			}
+			else
+				nodeB = nodeB->prev;
 		}
-		current = current->next;
 	}
 }
