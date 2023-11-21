63af1d9499a206dc744513b971dc0033f77a6fbc
othmane <othmane.alaouimekkaoui@gmail.com>
2023-11-21 07:19:49 -0800

    task 7

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,12 +7,12 @@
  */
 size_t get_max_gap(size_t size)
 {
-        size_t n;
+	size_t n;
 
-        n = 1;
-        while (n < size)
-                n = n * 3 + 1;
-        return ((n - 1) / 3);
+	n = 1;
+	while (n < size)
+		n = n * 3 + 1;
+	return ((n - 1) / 3);
 }
 
 /**
@@ -21,22 +21,22 @@ size_t get_max_gap(size_t size)
  * @size: the size of the array
  * Return: nothing
  */
-void shell_sort(int * array, size_t size)
+void shell_sort(int *array, size_t size)
 {
-        size_t gap, i, j;
-        int tmp;
+	size_t gap, i, j;
+	int tmp;
 
-        if (!array || !size)
-                return;
-        for (gap = get_max_gap(size); gap; gap = (gap - 1) / 3)
-        {
-                for (i = gap; i < size; i++)
-                {
-                        tmp = array[i];
-                        for (j = i; j > gap - 1 && array[j - gap] > tmp; j = j - gap)
-                                array[j] = array[j - gap];
-                        array[j] = tmp;
-                }
-                print_array(array, size);
-        }
+	if (!array || !size)
+		return;
+	for (gap = get_max_gap(size); gap; gap = (gap - 1) / 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			tmp = array[i];
+			for (j = i; j > gap - 1 && array[j - gap] > tmp; j = j - gap)
+				array[j] = array[j - gap];
+			array[j] = tmp;
+		}
+		print_array(array, size);
+	}
 }
--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,6 +1,54 @@
 #include "sort.h"
 
 /**
+ * merge_cmp - compare two elements before sorting
+ * @array: the array to sort
+ * @start: the index of the element to sort
+ * @stop: the index of the element wher to stop
+ * @new: the new array
+ * Return: nothing
+ */
+void merge_cmp(int *array, size_t start, size_t stop, int *new)
+{
+	size_t i = start;
+	size_t j, k, mid;
+
+	j = mid = (start + stop) / 2;
+	printf("Merging...\n");
+	printf("[left]: ");
+	print_array(array + start, mid - start);
+	printf("[right]: ");
+	print_array(array + mid, stop -  mid);
+	for (k = start; k < stop; k++)
+		if (i < mid && (j >= stop || array[i] <= array[j]))
+			new[k] = array[i++];
+		else
+			new[k] = array[j++];
+	printf("[Done]: ");
+	print_array(new + start, stop - start);
+}
+
+/**
+ * merge_top_down - sort both top and down
+ * @array: the array to sort
+ * @start: the top of the array
+ * @stop: the down of the array
+ * @new: the new output of the array
+ * Return: nothing
+ */
+void merge_top_down(int *array, size_t start, size_t stop, int *new)
+{
+	size_t mid;
+
+	mid = (start + stop) / 2;
+	if (stop - start < 2)
+		return;
+	merge_top_down(new, start, mid, array);
+	merge_top_down(new, mid, stop, array);
+	merge_cmp(array, start, stop, new);
+}
+
+/**
  * merge_sort - sorts an array of integers using the Merge sort algorithm
  * @array: the array to sort
  * @size: the size of the array
@@ -8,4 +56,16 @@
  */
 void merge_sort(int *array, size_t size)
 {
+	int *new;
+	size_t i;
 
+	if (!array || size < 2)
+		return;
+	new = malloc(sizeof(int) * size);
+	if (!new)
+		return;
+	for (i = 0; i < size; i++)
+		new[i] = array[i];
+	merge_top_down(new, 0, size, array);
+	free(new);
+}
