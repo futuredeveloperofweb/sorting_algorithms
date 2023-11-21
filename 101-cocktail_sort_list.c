7c1ad2f1991c1c8c6ca0dea5146c3a424020fc71
othmane <othmane.alaouimekkaoui@gmail.com>
2023-11-21 10:04:40 -0800

    task 8

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,10 +1,28 @@
 #include "sort.h"
 
-/**
- * cocktail_sort_list - sorts a list sing the Cocktail shaker sort alg
- * @list: the doubly linked list to sort
- * Return: nothing
- */
-void cocktail_sort_list(listint_t **list)
-{
+void swap_nodes(listint_t *node1, listint_t *node2) {
+    listint_t *temp = node1->next;
+    node1->next = node2;
+    node2->next = temp;
+}
 
+listint_t *create_listint(int *array, size_t n) {
+    listint_t *list = NULL;
+    for (size_t i = 0; i < n; i++) {
+        list = insert_node_end(list, array[i]);
+    }
+    return list;
+}
+
+void cocktail_sort_list(listint_t **list) {
+    listint_t *current, *next;
+    for (current = *list; current->next != NULL; current = current->next) {
+        next = current->next;
+        if (current->n > next->n) {
+            swap_nodes(current, next);
+        }
+        if (current->n < next->n) {
+            swap_nodes(current, next);
+        }
+    }
+}
--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,4 +1,70 @@
 #include "sort.h"
+#define parent(x) (((x) - 1) / 2)
+#define left(x) (((x) * 2) + 1)
+
+/**
+ * swap - swap two elements
+ * @array: the array to sort
+ * @size: the size of the array
+ * @A: the first element
+ * @B: the second element
+ * Return: nothing
+ */
+void swap(int *array, size_t size, int *A, int *B)
+{
+	if (*A != *B)
+	{
+		*A = *A + *B;
+		*B = *A - *B;
+		*A = *A - *B;
+		print_array(array, size);
+	}
+}
+
+/**
+ * sift - for  implementation
+ * @array: the array to sort
+ * @size: the size of the array
+ * @start: the beginning of the array
+ * @end: the end of the array
+ * Return: void
+ */
+void sift(int *array, size_t start, size_t end, size_t size)
+{
+	size_t root = start, s, ch;
+
+	while (left(root) <= end)
+	{
+		ch = left(root);
+		s = root;
+		if (array[s] < array[ch])
+			s = ch;
+		if (ch + 1 <= end && array[s] < array[ch + 1])
+			s = ch + 1;
+		if (s == root)
+			return;
+		swap(array, size, &array[root], &array[s]);
+		root = s;
+	}
+}
+
+/**
+ * heap - put the heap in place
+ * @array: the array to sort
+ * @size: size of the array
+ * Return: nothing
+ */
+void heap(int *array, size_t size)
+{
+	ssize_t start;
+
+	start = parent(size - 1);
+	while (start >= 0)
+	{
+		sift(array, start, size - 1, size);
+		start--;
+	}
+}
 
 /**
  * heap_sort - sorts an array of integers using the Heap sort algorithm
@@ -8,4 +74,16 @@
  */
 void heap_sort(int *array, size_t size)
 {
+	size_t end;
 
+	if (!array || !size)
+		return;
+	heap(array, size);
+	end = size - 1;
+	while (end > 0)
+	{
+		swap(array, size, &array[end], &array[0]);
+		end--;
+		sift(array, 0, end, size);
+	}
+}
