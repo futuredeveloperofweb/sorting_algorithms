154226fd06f9a4a1ec37a3d97188c69bc2f92785
othmane <othmane.alaouimekkaoui@gmail.com>
2023-11-21 14:54:48 -0800

    task 6

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,11 +1,30 @@
 #include "sort.h"
 
-/**
- * counting_sort - sorts an array of integer  using the Counting sort algo
- * @array: the array to sort
- * @size the size of the array
- * Return: nothing
- */
 void counting_sort(int *array, size_t size)
 {
+	int max = 0;
+	size_t i;
+	size_t k;
+	int index;
+	int *counting_array;
 
+	for (i = 0; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+	k = max + 1;
+	counting_array = (int *) malloc(k * sizeof(int));
+	for (i = 0; i < k; i++)
+		counting_array[i] = 0;
+
+	for (i = 0; i < size; i++)
+		counting_array[array[i]]++;
+
+	print_array(counting_array, k);
+	printf("\n");
+
+	index = 0;
+	for (i = 0; i < k; i++)
+		while (counting_array[i]-- > 0)
+			array[index++] = i;
+	free(counting_array);
+}
