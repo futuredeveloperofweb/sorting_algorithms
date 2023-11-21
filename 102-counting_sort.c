109645d39f865bca210f6acdfd543e33825c1570
othmane <othmane.alaouimekkaoui@gmail.com>
2023-11-21 15:09:59 -0800

    task 6

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,30 +1,49 @@
 #include "sort.h"
+#include <stdlib.h>
+#include <stdio.h>
 
+/**
+  * counting_sort - sort an array using counting algorithm.
+  * @array: array to sort.
+  * @size: the size of the array.
+  * Return: Nothing.
+  */
 void counting_sort(int *array, size_t size)
 {
-	int max = 0;
-	size_t i;
-	size_t k;
-	int index;
-	int *counting_array;
+	unsigned int i = 1;
+	int *cnt = NULL, k = 0, j = 0;
 
+	if (!array || size < 2)
+		return;
+
+	k = array[0];
+	for (; i < size; i++)
+		if (array[i] > k)
+			k = array[i];
+
+	cnt = malloc(sizeof(int) * (k + 1));
+	if (!cnt)
+		return;
+	for (j = 0; j <= k; j++)
+		cnt[j] = 0;
 	for (i = 0; i < size; i++)
-		if (array[i] > max)
-			max = array[i];
-	k = max + 1;
-	counting_array = (int *) malloc(k * sizeof(int));
-	for (i = 0; i < k; i++)
-		counting_array[i] = 0;
-
+		cnt[array[i]] += 1;
+	for (j = 0; j < k; j++)
+	{
+		cnt[j + 1] += cnt[j];
+		printf("%d, ", cnt[j]);
+	}
+	cnt[j + 1] += cnt[j];
+	printf("%d\n", cnt[j + 1]);
 	for (i = 0; i < size; i++)
-		counting_array[array[i]]++;
-
-	print_array(counting_array, k);
-	printf("\n");
-
-	index = 0;
-	for (i = 0; i < k; i++)
-		while (counting_array[i]-- > 0)
-			array[index++] = i;
-	free(counting_array);
+	{
+		j = cnt[array[i]] - 1;
+		if (array[i] != array[j])
+		{
+			k = array[i];
+			array[i] = array[j];
+			array[j] = k;
+		}
+	}
+	free(cnt);
 }
